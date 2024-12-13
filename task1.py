d0d9db2626d7b5761543ae0b3c5710dd85b9316d
hohlayder <106448703+hohlayder@users.noreply.github.com>
2024-12-13 21:10:57 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,12 +1,12 @@
 class Book:
-    title = author = year = None
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
 
     def get_info(self):
         return f'Название книги: {self.title}, Автор: {self.author}, Год издания: {self.year}'
 
 
-b = Book()
-b.title = 'Последнее желание'
-b.author = 'Анджей Сапковский'
-b.year = 1993
+b = Book('Последнее желание', 'Анджей Сапковский', 1993)
 print(b.get_info())
